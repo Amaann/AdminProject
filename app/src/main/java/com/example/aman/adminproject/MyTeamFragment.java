707c7bf274b71a5cdb2e4e483c0083bf5c58bc59
aman <amankumarbostan@gmail.com>
2018-06-27 11:08:18 +0530

    added circleImageView and toolTip (longPress) function in fragament_my_team layout

--- app/src/main/java/com/example/aman/adminproject/MyTeamFragment.java
+++ app/src/main/java/com/example/aman/adminproject/MyTeamFragment.java
@@ -2,18 +2,24 @@ package com.example.aman.adminproject;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.TooltipCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class MyTeamFragment extends Fragment {
 
     View v;
     TextView textView;
+    CircleImageView circleImageView;
+    CircleImageView circleImageView2;
 
     public MyTeamFragment() {
         // Required empty public constructor
@@ -30,14 +36,40 @@ public class MyTeamFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         v = inflater.inflate(R.layout.fragment_my_team, container, false);
-//        initView();
+        initView();
         return v;
     }
-////
-////    private void initView() {
-////        textView=v.findViewById(R.id.textViewMyTeam);
-////        textView.setText("My Team");
-//    }
+
+    private void initView() {
+        SetcircleImageView();
+        setCircleImageView2();
+
+
+    }
+
+    public void SetcircleImageView() {
+
+        circleImageView = v.findViewById(R.id.Id_IcV_CircleOne);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            circleImageView.setTooltipText("Aman Kumar, System Admin, 7022266557");
+        } else {
+            TooltipCompat.setTooltipText(circleImageView, "Aman Kumar, System Admin, 7022266557");
+        }
+
+    }
+
+    public void setCircleImageView2() {
+
+        circleImageView2 = v.findViewById(R.id.Id_IcV_CircleTwo);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            circleImageView2.setTooltipText("Ritesh, System Admin, 7022266557");
+        } else {
+            TooltipCompat.setTooltipText(circleImageView2, "Ritesh, System Admin, 7022266557");
+        }
+
+    }
 
     @Override
     public void onAttach(Context context) {
