84911da42d166b5f690b1e1fceca68bde243b217
aman <amankumarbostan@gmail.com>
2018-07-13 17:40:06 +0530

    added array in all spinner

--- app/src/main/java/com/example/aman/adminproject/CreatesTicketFragment.java
+++ app/src/main/java/com/example/aman/adminproject/CreatesTicketFragment.java
@@ -1,7 +1,5 @@
 package com.example.aman.adminproject;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,17 +7,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class CreatesTicketFragment extends Fragment {
 
+    Spinner spinnerAssignedto, spinnerWatchers, spinnerTicketSlot, spinnerSelectStore;
+    ArrayAdapter<CharSequence> arrayAdapterAssignedto, arrayAdapterWatchers, arrayAdapterTicketSlot, arrayAdapterSelectStore;
+
     View v;
 
-
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_creates_ticket, container, false);
+
+        initView();
+
         return v;
+
+
     }
+
+    private void initView() {
+        spinnerWatchers = v.findViewById(R.id.Id_Spinner_Watchers);
+        spinnerAssignedto = v.findViewById(R.id.Id_Spinner_AssignedTo);
+        spinnerTicketSlot = v.findViewById(R.id.Id_Spinner_TicketSlot);
+        spinnerSelectStore = v.findViewById(R.id.Id_Spinner_SelectStore);
+
+        arrayAdapterWatchers = ArrayAdapter.createFromResource(getActivity(), R.array.watchers, android.R.layout.simple_spinner_item);
+        arrayAdapterWatchers.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerWatchers.setAdapter(arrayAdapterWatchers);
+
+
+        arrayAdapterAssignedto = ArrayAdapter.createFromResource(getActivity(), R.array.assignedTo, android.R.layout.simple_spinner_item);
+        arrayAdapterAssignedto.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerAssignedto.setAdapter(arrayAdapterAssignedto);
+
+
+        arrayAdapterTicketSlot = ArrayAdapter.createFromResource(getActivity(),R.array.ticketSlot, android.R.layout.simple_spinner_item);
+        arrayAdapterTicketSlot.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerTicketSlot.setAdapter(arrayAdapterTicketSlot);
+
+        arrayAdapterSelectStore = ArrayAdapter.createFromResource(getActivity(), R.array.Warehouse, android.R.layout.simple_spinner_item);
+        arrayAdapterSelectStore.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerSelectStore.setAdapter(arrayAdapterSelectStore);
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/aman/adminproject/SearchTicktesFragment.java
+++ app/src/main/java/com/example/aman/adminproject/SearchTicktesFragment.java
@@ -53,19 +53,6 @@ public class SearchTicktesFragment extends Fragment {
         arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                Toast.makeText(getContext(), parent.getItemIdAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
     }
 
 
