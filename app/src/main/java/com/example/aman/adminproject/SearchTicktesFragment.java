b623a58b5823d31b8914c152793a4199e43f2a49
aman <amankumarbostan@gmail.com>
2018-07-13 13:26:45 +0530

    added item in spinner

--- app/src/main/java/com/example/aman/adminproject/SearchTicktesFragment.java
+++ app/src/main/java/com/example/aman/adminproject/SearchTicktesFragment.java
@@ -4,17 +4,29 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class SearchTicktesFragment extends Fragment {
+
+    View v;
+    Spinner spinner;
+    Context context;
+
+    ArrayAdapter<CharSequence> arrayAdapter;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
@@ -23,9 +35,38 @@ public class SearchTicktesFragment extends Fragment {
                              Bundle savedInstanceState) {
 
 
-    // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_tickets,container,false);
-}
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_search_tickets, container, false);
+        initView();
+        return v;
+
+
+    }
+
+    private void initView() {
+
+        spinner = v.findViewById(R.id.Id_Wh_Spinner);
+
+        arrayAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.Warehouse,
+                android.R.layout.simple_spinner_item);
+
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getContext(), parent.getItemIdAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
 
 
     @Override
@@ -38,5 +79,6 @@ public class SearchTicktesFragment extends Fragment {
     public void onDetach() {
 
         super.onDetach();
-    }}
+    }
+}
 
